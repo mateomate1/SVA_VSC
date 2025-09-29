aa9a93fadad915ac58083adb15f2c34ef1466d01
mateomate1 <mateoayarrabarbero@gmail.com>
2025-09-29 10:43:46 +0200

    git init

---
+++ src/es/ciudadescolar/Programa.java
@@ -0,0 +1,20 @@
+package es.ciudadescolar;
+public class Programa {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        // TODO: añadir siempre a fichero log las acciones más importantes
+        // FIXME: 
+        // HACK: 
+        // BUG: 
+        // OPTIMIZE: 
+        // NOTE: 
+        // REVIEW: 
+        // DEPRECATED: 
+        // IMPORTANT: 
+        // WARNING: 
+        // XXX:
+        // TEMP: 
+        // QUESTION: 
+        // UNDONE:
+    }
+}
